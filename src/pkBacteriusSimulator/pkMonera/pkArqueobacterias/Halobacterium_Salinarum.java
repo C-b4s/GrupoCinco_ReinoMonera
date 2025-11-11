a2e1e71d224148f46c726d8c6287405f0fb53568
gomu546 <sornozabolagaymichael@gmail.com>
2025-11-11 18:08:46 -0500

    hice la parte de el halobacteruim que debe ir relacionado con la otra bacteria de la clasificacion delas arqueobacterias

--- src/pkBacteriusSimulator/pkMonera/pkArqueobacterias/Halobacterium_Salinarum.java
+++ src/pkBacteriusSimulator/pkMonera/pkArqueobacterias/Halobacterium_Salinarum.java
@@ -1,5 +1,42 @@
 package pkBacteriusSimulator.pkMonera.pkArqueobacterias;
 
-public class Halobacterium_Salinarum extends Arqueobacterias{
+public class Halobacterium_Salinarum extends Arqueobacterias {
+
+    private Double nivelSalinidadOptima;
+
+    public Halobacterium_Salinarum(String nombreCientifico, String habitat, Double nivelSalinidadOptima) {
+        super(nombreCientifico, habitat);
+        this.nivelSalinidadOptima = nivelSalinidadOptima;
+    }
+
+    public Double getNivelSalinidadOptima() {
+        return nivelSalinidadOptima;
+    }
+
+    public void setNivelSalinidadOptima(Double nivelSalinidadOptima) {
+        this.nivelSalinidadOptima = nivelSalinidadOptima;
+    }
+
+    public String analizarRespuestaSalina() {
+        if (nivelSalinidadOptima > 3.0) {
+            return "Alta adaptación a ambientes hipersalinos.";
+        } else if (nivelSalinidadOptima >= 1.5) {
+            return "Adaptación moderada a la salinidad.";
+        } else {
+            return "Baja tolerancia a condiciones salinas.";
+        }
+    }
+
+    public String compararAdaptacion(String bacteria) {
+        switch (bacteria.toLowerCase()) {
+            case "methanococcus jannaschii":
+                return "Methanococcus jannaschii se adapta mejor a condiciones anaeróbicas, " +
+                        "mientras que Halobacterium salinarum destaca en ambientes hipersalinos.";
+            case "streptococcus":
+                return "Halobacterium salinarum presenta mayor tolerancia salina que Streptococcus.";
+            default:
+                return "No hay datos suficientes para comparar con " + bacteria + ".";
+        }
+    }
 
 }

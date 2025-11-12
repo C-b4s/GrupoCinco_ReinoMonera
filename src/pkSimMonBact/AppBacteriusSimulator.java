6bc4ec22c660bb9d0c41e4611f26c7da5a596115
robinn_yk <robinsonalessandro3@hotmail.com>
2025-11-11 22:34:24 -0500

    Arreglando la clase bacteria Clostridum para que cuadre con lo demas

--- src/pkSimMonBact/AppBacteriusSimulator.java
+++ src/pkSimMonBact/AppBacteriusSimulator.java
@@ -8,7 +8,7 @@ public class AppBacteriusSimulator {
     public void inicializar(){
         imprimirBienvenida();
         Bacteriologo bacteriologo1 = new Bacteriologo();
-        MicrobiologoAmbiental microBioAmb1 =new MicrobiologoAmbiental("null", null, null, null, null, null, null, null, null, null)
+        MicrobiologoAmbiental microBioAmb1 =new MicrobiologoAmbiental("null", null, null, null, null, null, null, null, null, null);
         microBioAmb1.analizarRespuestaSalinidad("");
          Halobacterium_Salinarum halo1=new Halobacterium_Salinarum(null, null, 23.3);
 
